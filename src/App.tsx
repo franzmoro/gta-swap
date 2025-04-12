8145cb36b9804ec6204aef47f97efca9cc96ee3d
Franz Moro <franzmoro@users.noreply.github.com>
2025-04-12 23:58:27 +0100

    feat: remove page wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import SwapWidget from './screens/swap';
 
 const App = () => {
-  return (
-    <main className="flex min-h-screen flex-col items-center bg-background">
-      <SwapWidget />
-    </main>
-  );
+  return <SwapWidget />;
 };
 
 export default App;
